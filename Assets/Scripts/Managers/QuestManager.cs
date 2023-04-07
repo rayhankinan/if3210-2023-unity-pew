850f6013a57220b398bc12e6f09ccd25c2174fc5
rayhankinan <rayhankinan@gmail.com>
2023-04-08 00:01:38 +0700

    Fix Healthbar Visibility

--- Assets/Scripts/Managers/QuestManager.cs
+++ Assets/Scripts/Managers/QuestManager.cs
@@ -4,35 +4,36 @@ using UnityEngine.SceneManagement;
 
 public class QuestManager : MonoBehaviour
 {
-    public static Objective[] currentObjectives; // TODO: DIGUNAKAN PADA GUI UNTUK MELIHAT PROGRESS QUEST
+    private static Objective[] _currentObjectives; // TODO: DIBUAT MENJADI PUBLIC UNTUK GUI MELIHAT QUEST
     
     public float restartDelay = 5f;
+    public string nextScene = "main_menu";
     
     private Animator _anim;
     private float _restartTimer;
 
     private void Awake()
     {
-        currentObjectives = GetComponents<Objective>();
+        _currentObjectives = GetComponents<Objective>();
         _anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if (!currentObjectives.All(objective => objective.IsCompleted())) return;
+        if (!_currentObjectives.All(objective => objective.IsCompleted())) return;
         
         // TODO: SET TRIGGER UNTUK ANIMASI MEMBERESKAN QUEST
         _restartTimer += Time.deltaTime;
 
         if (_restartTimer >= restartDelay)
         {
-            SceneManager.LoadScene("main_menu");
+            SceneManager.LoadScene(nextScene);
         }
     }
 
     public static void AddEnemy(GameObject killedEnemy)
     {
-        foreach (var currentObjective in currentObjectives)
+        foreach (var currentObjective in _currentObjectives)
             if (!currentObjective.IsCompleted())
             {
                 currentObjective.AddEnemy(killedEnemy);
