6328805b3529750d8536fdbfea76e0f9544f9dca
davidkarelh <kareldavid9@gmail.com>
2023-04-11 17:39:38 +0700

    Add shop info text

--- Assets/Scripts/Managers/ShopKeeperManager.cs
+++ Assets/Scripts/Managers/ShopKeeperManager.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class ShopKeeperManager : MonoBehaviour
 {
     private static readonly int BuyClick = Animator.StringToHash("BuyClick");
+    private static readonly int BuyErrorClick = Animator.StringToHash("BuyErrorClick");
 
+    public GameObject shopKeeper;
     private Animator _anim;
+    private ShopKeeperEffect _shopKeeperEffect;
 
     // Start is called before the first frame update
     void Start()
     {
         _anim = GetComponent<Animator>();
+        _shopKeeperEffect = shopKeeper.GetComponent<ShopKeeperEffect>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_shopKeeperEffect.isShopKeeperFlying)
+        {
+            _anim.SetBool("ShopKeeperFlying", true);
+        }
+        else
+        {
+            _anim.SetBool("ShopKeeperFlying", false);
+        }
+
         if (Input.GetKey(KeyCode.B))
         {
-            _anim.SetTrigger(BuyClick);
+            if (!_shopKeeperEffect.isShopKeeperFlying)
+            {
+                _anim.SetTrigger(BuyErrorClick);
+            }
         }
     }
 }
