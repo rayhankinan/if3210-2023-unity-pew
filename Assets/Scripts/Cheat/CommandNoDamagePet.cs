76484720560f98141dfe53031c4a2849f1c831d4
nurulutamiaw <13517132@std.stei.itb.ac.id>
2023-04-14 20:19:59 +0700

    construct cheat for pet

---
+++ Assets/Scripts/Cheat/CommandNoDamagePet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandNoDamagePet : CheatCommandBase
+{
+    public override string commandID { get; protected set; }
+    public override string commandDescription { get; protected set; }
+
+    public CommandNoDamage()
+    {
+        commandID = "no_damage_pet";
+        commandDescription = "Enemy attacks will not affect to pet health";
+        
+        AddCommandToConsole();
+    }
+
+    public override void RunCommand()
+    {
+        
+    }
+
+    public static CommandNoDamagePet CreateCommand()
+    {
+        return new CommandNoDamagePet();
+    }
+}
