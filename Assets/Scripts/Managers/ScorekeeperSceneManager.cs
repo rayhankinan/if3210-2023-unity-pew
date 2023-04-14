fc03a5fb5e16dc9f9b1410fa86cb2f98062584b5
Wesly Giovano <weslygio@yahoo.com>
2023-04-15 01:13:59 +0700

    add shopkeeper scene load

---
+++ Assets/Scripts/Managers/ScorekeeperSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScorekeeperSceneManager : MonoBehaviour
+{
+    public float timeLimit;
+    float _startTime;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+        _startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Time.time - _startTime > timeLimit)
+        {
+            var nextScene = CurrentStateData.GetCurrentScene();
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
