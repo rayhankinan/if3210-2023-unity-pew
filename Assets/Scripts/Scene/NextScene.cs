df4dfac8c92e2341fb49771b95b73a539bf46109
OjaanIr <13520067@std.stei.itb.ac.id>
2023-04-15 16:03:13 +0700

    add transition

--- Assets/Scripts/Scene/NextScene.cs
+++ Assets/Scripts/Scene/NextScene.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class NextScene: MonoBehaviour
 {
+    public Animator anim;
     public string scene;
 
     public void NextWithoutSave()
     {
         if (CurrentStateData.GetCurrentScene() != "level_04")
         {
+            StartCoroutine(Crossfade());
             SceneManager.LoadScene("cutscene_shopkeeper_turun");
         }
         CurrentStateData.ChangeScene(scene);
+        StartCoroutine(Crossfade());
     }
 
     public void NextWithSave()
@@ -23,6 +28,14 @@ public class NextScene: MonoBehaviour
     public void NextWithoutShopkeeper()
     {
         CurrentStateData.ChangeScene(scene);
+        StartCoroutine(Crossfade());
         SceneManager.LoadScene(scene);
+    }
+
+    IEnumerator Crossfade()
+    {
+        Debug.Log("Start");
+        anim.SetTrigger("Start");
+        yield return new WaitForSeconds(1f);
     }
 }
