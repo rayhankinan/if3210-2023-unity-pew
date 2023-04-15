28f18f7dc6f41c271c27972253a8784a10d94532
nurulutamiaw <13517132@std.stei.itb.ac.id>
2023-04-15 13:35:57 +0700

    update cheats for pet

--- Assets/Scripts/Cheat/CommandNoDamagePet.cs
+++ Assets/Scripts/Cheat/CommandNoDamagePet.cs
@@ -6,7 +6,12 @@ public class CommandNoDamagePet : CheatCommandBase
 {
     public override string commandID { get; protected set; }
     public override string commandDescription { get; protected set; }
-    
+
+    private GameObject petDragon; /*= GameObject.FindGameObjectWithTag("Pet Dragon");*/
+    private GameObject petWizard; /*= GameObject.FindGameObjectWithTag("Aura Buff Wizard");*/
+    private PetDragonHealth petDragonHealth;
+    private PetWizardHealth petWizardHealth;
+    private int currentPet;
 
     public CommandNoDamagePet()
     {
@@ -18,7 +23,35 @@ public class CommandNoDamagePet : CheatCommandBase
 
     public override void RunCommand()
     {
-        
+        currentPet = CurrentStateData.GetCurrentPet();
+        if (currentPet == 0 || currentPet == 2)
+        {
+            petWizard = GameObject.FindGameObjectWithTag("Aura Buff Wizard");
+            petWizardHealth = petDragon.GetComponent<PetWizardHealth>();
+            petWizardHealth.Immortal();
+        }
+        else if (currentPet == 1)
+        {
+            petDragon = GameObject.FindGameObjectWithTag("Pet Dragon");
+            petDragonHealth = petDragon.GetComponent<PetDragonHealth>();
+            petDragonHealth.Immortal();
+        }
+        /*petDragonHealth = petDragon.GetComponentInChildren<PetDragonHealth>();
+        petWizardHealth = petWizard.GetComponentInChildren<PetWizardHealth>();
+        if (petDragon.gameObject.activeInHierarchy)
+        {
+            currentPet = 1;
+        }
+        else if (petWizard.gameObject.activeInHierarchy)
+        {
+            currentPet = 2;
+            petWizardHealth.Immortal();
+        }
+        else
+        {
+            currentPet = -1;
+        }
+        Debug.Log(currentPet);*/
     }
 
     public static CommandNoDamagePet CreateCommand()
